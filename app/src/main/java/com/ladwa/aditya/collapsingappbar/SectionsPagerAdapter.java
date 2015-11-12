6e00b009e5373309375de55c2c157906b0f6af99
LadwaAditya <ladwa.aditya@gmail.com>
2015-11-12 17:59:53 +0530

    Initial Commit: Created basic Layout

---
+++ app/src/main/java/com/ladwa/aditya/collapsingappbar/SectionsPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.ladwa.aditya.collapsingappbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Aditya on 12-Nov-15.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private static final String[] TITLES = {"SECTION 1", "SECTION 2", "SECTION 3"};
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return SimpleFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return TITLES.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position >= 0 && position < TITLES.length) {
+            return TITLES[position];
+        }
+        return null;
+    }
+}
